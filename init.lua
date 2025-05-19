b7a77119cb7dd45283c0d2e5365173131ffc8244
charlie <cracken>
2025-05-18 18:11:44 -0600

    feat: add avante and load .env

--- init.lua
+++ init.lua
@@ -1,25 +1,23 @@
---
--- -- Treesitter
--- later(function()
---     add({
---         source = "nvim-treesitter/nvim-treesitter",
---         -- Use 'master' while monitoring updates in 'main'
---         checkout = "master",
---         monitor = "main",
---         -- Perform action after every checkout
---         hooks = {
---             post_checkout = function()
---                 vim.cmd("TSUpdate")
---             end,
---         },
---     })
---     -- Possible to immediately execute code which depends on the added plugin
---     require("nvim-treesitter.configs").setup({
---         ensure_installed = { "lua", "vimdoc" },
---         highlight = { enable = true },
---     })
--- end)
---
+-- Load env variables from .config/nvim
+local env_file = vim.fn.stdpath("config") .. "/.env"
+
+local function load_env_file(path)
+    local file = io.open(path, "r")
+    if not file then
+        return
+    end
+
+    for line in file:lines() do
+        local key, val = line:match("^([%w_]+)%s*=%s*(.+)$")
+        if key and val then
+            vim.fn.setenv(key, val)
+        end
+    end
+
+    file:close()
+end
+
+load_env_file(env_file)
 -- AI
 -- add({
 --     source = "yetone/avante.nvim",
@@ -99,6 +97,75 @@ require("lazy").setup({
             build = "TSUpdate",
         },
         { "ellisonleao/gruvbox.nvim" },
+        { "ibhagwan/fzf-lua" },
+        {
+            "yetone/avante.nvim",
+            event = "VeryLazy",
+            version = false, -- Never set this value to "*"! Never!
+            opts = {
+                -- add any opts here
+                -- for example
+                -- provider = "openai",
+                -- openai = {
+                --     endpoint = "https://api.openai.com/v1",
+                --     model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+                --     timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+                --     temperature = 0,
+                --     max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+                --     --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+                -- },
+                provider = "deepseek",
+                vendors = {
+                    deepseek = {
+                        __inherited_from = "openai",
+                        api_key_name = "DEEPSEEK_API_KEY",
+                        endpoint = "https://api.deepseek.com",
+                        model = "deepseek-coder",
+                    },
+                },
+            },
+            -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+            build = "make",
+            -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+            dependencies = {
+                "nvim-treesitter/nvim-treesitter",
+                "stevearc/dressing.nvim",
+                "nvim-lua/plenary.nvim",
+                "MunifTanjim/nui.nvim",
+                --- The below dependencies are optional,
+                "echasnovski/mini.pick", -- for file_selector provider mini.pick
+                -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+                -- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+                "ibhagwan/fzf-lua", -- for file_selector provider fzf
+                -- "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+                -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+                {
+                    -- support for image pasting
+                    "HakonHarnes/img-clip.nvim",
+                    event = "VeryLazy",
+                    opts = {
+                        -- recommended settings
+                        default = {
+                            embed_image_as_base64 = false,
+                            prompt_for_file_name = false,
+                            drag_and_drop = {
+                                insert_mode = true,
+                            },
+                            -- required for Windows users
+                            use_absolute_path = true,
+                        },
+                    },
+                },
+                {
+                    -- Make sure to set this up properly if you have lazy=true
+                    "MeanderingProgrammer/render-markdown.nvim",
+                    opts = {
+                        file_types = { "markdown", "Avante" },
+                    },
+                    ft = { "markdown", "Avante" },
+                },
+            },
+        },
 
         -- add your plugins here
     },
@@ -122,9 +189,6 @@ require("mini.indentscope").setup()
 require("mini.fuzzy").setup()
 
 require("mini.files").setup()
-vim.keymap.set("n", "<leader>e", function()
-    require("mini.files").open()
-end)
 
 require("mini.completion").setup({
     mappings = {
@@ -298,4 +362,7 @@ require("nvim-treesitter.configs").setup({
     highlight = { enable = true },
 })
 
-vim.keymap.set("n", "<leader>e", ":nohl<CR>", { noremap = true })
+--- MAPPINGS
+require("mappings")
+
+-- FUNCTIONS
