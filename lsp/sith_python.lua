263b45703d4c6d0a5712f76163fb670397547de6
🦈 Carlos Solórzano Cerdas <csolorzano@istmocenter.com>
2025-05-19 08:37:19 -0600

    fix: sith sith language server options

--- lsp/sith_python.lua
+++ lsp/sith_python.lua
@@ -1,21 +1,26 @@
+local pwd = vim.loop.cwd()
 return {
     cmd = { "/home/charlie/.local/bin/sith-lsp" },
     single_file_support = true,
     filetypes = { "python" },
     root_markers = {
-
         "pyproject.toml",
         "requirements.txt",
         "Pipfile",
         "pyrightconfig.json",
         ".git",
     },
-    settings = {
-        -- Settings for the server goes here.
-        -- Config example
-        ruff = {
-            lint = {
-                enable = true,
+
+    root_dir = pwd,
+
+    init_options = {
+        settings = {
+            -- Settings for the server goes here.
+            -- Config example
+            ruff = {
+                lint = {
+                    enable = true,
+                },
             },
         },
     },
